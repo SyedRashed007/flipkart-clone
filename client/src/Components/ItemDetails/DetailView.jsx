54ebd65842c90e7e1e031854fa5f77acea776771
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-08-09 20:52:00 +0530

    Initial

--- client/src/Components/ItemDetails/DetailView.jsx
+++ client/src/Components/ItemDetails/DetailView.jsx
@@ -22,6 +22,7 @@ const useStyles = makeStyles(theme => ({
     },
     rightContainer: {
         marginTop: 50,
+        textAlign: 'left',
         '& > *': {
             marginTop: 10
         }
